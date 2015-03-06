66aecc50d5fb06feda7078df0863d471acd17dc9
zulman <zulman@bk.ru>
2015-03-06 15:57:31 +0300

    till last man stand

---
+++ main.go
@@ -0,0 +1,9 @@
+// coming soon by industry's mad veterans
+// stay strong, Voronov is coming...
+// 06.03.2015
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("Wanna play a game by Dmitry Voronov?")
+}
